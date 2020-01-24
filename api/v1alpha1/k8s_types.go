8bc25c7d60abb4030e6e18a756c4a0f39d92d44f
Holden Omans <holden.omans@gmail.com>
2020-01-24 09:12:05 -0500

    Update doc

--- api/v1alpha1/k8s_types.go
+++ api/v1alpha1/k8s_types.go
@@ -106,7 +106,7 @@ func (v *VolumeSpec) Validate() (ok bool, err error) {
 // PodConfigurationSpec contains the configuration for the benchmark pods
 type PodConfigurationSpec struct {
 
-	// Annotations is an unstructured key value map stored with a resource that may be
+	// PodAnnotations is an unstructured key value map stored with a resource that may be
 	// set by external tools to store and retrieve arbitrary metadata. They are not
 	// queryable and should be preserved when modifying objects.
 	// More info: http://kubernetes.io/docs/user-guide/annotations
